1169a753ca7fa3239a33a7e7bcb09ebbaa06d2aa
Daniel Liberman <dandosh5090@gmail.com>
2023-09-10 14:58:28 +0300

    fixing bugs of one commit before

--- Components/frontend_service.py
+++ Components/frontend_service.py
@@ -16,14 +16,15 @@ class Service:
         front_Model = {}
         if self.cluster.find_In_Collection_By_Id(self.collection_name, int(graph_Id)) is not None:
             result = self.cluster.find_In_Collection_By_Id(self.collection_name, int(graph_Id))
-            front_Model['graph data'] = result['graph data']
+            front_Model['graph_data'] = result['graph_data']
             base64_image = base64.b64encode(result["graph_data"]).decode('utf-8')
             front_Model['graph_data'] = base64_image
             table_points_model = dict(X_name=result['X_Name'], X_array=result['X_Variables'],
                                       Y_name=result['Y_Name'], Y_array=result['Y_Variables'])
             front_Model['points_model'] = table_points_model
         else:
-            front_Model = {'correlation_coefficient': -2, 'prediction_percentage': -2, 'graph_data': 'error'}
+            # front_Model = {'correlation_coefficient': -2, 'prediction_percentage': -2, 'graph_data': 'error'}
+            pass
         return front_Model
 
     def sendListForTable(self):  # the function takes the data from mongo and create arrays of table model for angular.
